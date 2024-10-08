2486d69405863acd50eda3f78bc78591b8d83717
Richard(Kim,ByoungChul) <skyksit@gmail.com>
2024-10-08 16:01:01 +0900

    add slow motion update apps version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath (libs.kotlin.gradle)
-        classpath (libs.google.services)
-        classpath (libs.firebase.crashlytics.gradle)
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.10")
         // Add hilt-dagger
-        classpath (libs.dagger.hilt.android.gradle)
-        classpath (libs.kotlin.serialization)
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.51")
+        classpath ("org.jetbrains.kotlin:kotlin-serialization:2.0.10")
     }
 }
 
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.devtoolsKsp) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.10" apply false
+    id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
+    id("org.jetbrains.compose") version "1.6.11" apply false
 }
 
 tasks.register("clean", Delete::class) {
